930c142cf4a2a79eb4cc96a4822698668b218c1f
Worawan Diaz Carballo <wdc@cs.tu.ac.th>
2018-08-18 17:03:31 +0700

    initial commit

--- fetchWeatherData.js
+++ fetchWeatherData.js
@@ -1,7 +1,8 @@
-if (
-    !process.env.WCD_APIKEY
-)
+if (!process.env.WCD_APIKEY)
 {
     console.error("env for Weather Company Data not found");
     process.exit(1);
+} else
+{
+    console.log(`${WCD_APIKEY}`);
 }
